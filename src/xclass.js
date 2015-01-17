6efc3067f005b3ea1209e3544cbed1a8ca0bab7d
ayecue <soerenwehmeier@googlemail.com>
2015-01-17 03:37:22 +0100

    fixed getClass fn

--- src/xclass.js
+++ src/xclass.js
@@ -1,8 +1,13 @@
+var config = require('cls/config'),
+	getClass = require('fn/getClass');
+
 module.exports = {
 	forEach : require('fn/forEach'),
 	toArray : require('fn/toArray'),
 	extend : require('fn/extend'),
 	printf : require('fn/printf'),
-	getClass : require('fn/getClass'),
+	getClass : function(path){
+		return getClass(config.classPool,path);
+	},
 	define : require('cls/factory')
 };
